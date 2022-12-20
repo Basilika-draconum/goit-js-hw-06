286bc1f08548333f0268556cd3e8491d07a3d8ca
Lika <14dubinenkoanzhelika@gmail.com>
2022-12-20 22:34:46 +0200

    add task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsArray = ingredients.map(ingredient => {
+  const ingredientListItem = document.createElement('li');
+  ingredientListItem.textContent = ingredient;
+  ingredientListItem.classList.add('item');
+  return ingredientListItem;
+}); 
+ingredientsList.append(...ingredientsArray);
