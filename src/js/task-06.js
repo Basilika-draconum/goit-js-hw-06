b65083e4c849ff6c8e01852d7ff9bd56a68298b1
Lika <14dubinenkoanzhelika@gmail.com>
2022-12-21 22:23:32 +0200

    add task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,10 @@
+const inputForValidation = document.querySelector('#validation-input');
+
+inputForValidation.addEventListener("blur", () => {
+    const dataLength = inputForValidation.getAttribute("data-length");
+    const value = inputForValidation.value;
+    inputForValidation.classList.remove("invalid", "valid");
+    if (value.length < dataLength ) {inputForValidation.classList.add("invalid");
+    } else {inputForValidation.classList.add("valid");
+    }
+});
