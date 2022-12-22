88ac2e60df41cd4a78a6f1b4de1dee3e3b0cb426
Lika <14dubinenkoanzhelika@gmail.com>
2022-12-22 20:18:58 +0200

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handleLoginFormSubmit);
 
+let outputLoginForm = {};
+
+function handleLoginFormSubmit(e) {
+    e.preventDefault();
+    const { elements: { email, password } } = e.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    } else {
+        outputLoginForm.email = email.value;
+        outputLoginForm.password = password.value;
+        console.log(outputLoginForm);
+    }
+    e.currentTarget.reset();
+};
