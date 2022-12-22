9ce1f540b53c63fad7a9779c368105a4ac4ed212
Lika <14dubinenkoanzhelika@gmail.com>
2022-12-22 21:20:18 +0200

    add task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const randomHexColor = document.querySelector('.color');
+const buttonForChangeColor = document.querySelector('.change-color');
+
+buttonForChangeColor.addEventListener('click', (e) => {
+  let value = e.target.value;
+  e.target.value = getRandomHexColor(); 
+  randomHexColor.textContent = value;
+  document.body.style.backgroundColor = value;
+}); 
