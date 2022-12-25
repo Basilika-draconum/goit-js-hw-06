e8687a4694353ec801d8b88ff903becbe554c03c
Lika <14dubinenkoanzhelika@gmail.com>
2022-12-25 13:36:25 +0200

    fix task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,7 +4,7 @@ inputForValidation.addEventListener("blur", () => {
     const dataLength = inputForValidation.getAttribute("data-length");
     const value = inputForValidation.value;
     inputForValidation.classList.remove("invalid", "valid");
-    if (value.length < dataLength ) {inputForValidation.classList.add("invalid");
+    if (value.length < dataLength || value.length > dataLength) {inputForValidation.classList.add("invalid");
     } else {inputForValidation.classList.add("valid");
     }
 });
